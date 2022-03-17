e356236cef61c18661aabd85c32edb1a8f04f12a
leroux95t <97245084+leroux95t@users.noreply.github.com>
2022-03-17 14:14:00 +0100

    Add files via upload

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,10 +1,14 @@
 # TODO : Complete this
 from sklearn.base import BaseEstimator
+from sklearn.linear_model import LogisticRegression
+
+
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        pass
+        self.model = LogisticRegression(max_iter=500)
     def fit(self, X, y):
-    	pass
+        self.model.fit(X, y)
+
     def predict_proba(self, X):
-    	pass
+    	return self.model.predict_proba(X)
