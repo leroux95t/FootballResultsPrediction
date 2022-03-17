72471254f5f0112b938c8edae0f7dd04c18ec0f4
leroux95t <97245084+leroux95t@users.noreply.github.com>
2022-03-17 14:13:38 +0100

    Add files via upload

--- problem.py
+++ problem.py
@@ -6,10 +6,13 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 
 
-problem_title = 'FootballResults5League'
-_target_column_name = 'FTR'
 
-_prediction_label_names = ['H', 'A', 'D']
+
+
+problem_title = 'FootballResults'
+_target_column_name = 'Result'
+
+_prediction_label_names = ['0', '1', '2']
 
 Predictions = rw.prediction_types.make_multiclass(label_names=_prediction_label_names)
 
@@ -19,20 +22,24 @@ score_types = [
     rw.score_types.Accuracy(name='acc'),
 ]
 
-def _read_data(path, f_name):
-    data = pd.read_csv(os.path.join(path, f_name), sep=',')
-    y_array = data['FTR'].values
-    X_df = data.drop(['FTR'], axis=1)
-    return X_df, y_array
 
-def get_train_data(path='./data/'):
+def _read_data(path, f_name):
+    
+    data = pd.read_csv(os.path.join(path, 'data', f_name))
+    y_array = data['Result'].values
+    X_df = data.drop(['Result'], axis=1)
+
+    X_array = X_df.to_numpy()
+    return X_array, y_array
+
+def get_train_data(path='.'):
     f_name = 'data_train.csv'
     return _read_data(path, f_name)
 
-def get_test_data(path='./data/'):
+def get_test_data(path='.'):
     f_name = 'data_test.csv'
     return _read_data(path, f_name)
 
 def get_cv(X, y):
-    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.20, random_state=42)
+    cv = StratifiedShuffleSplit(n_splits=3, test_size=0.20, random_state=42)
     return cv.split(X, y)
